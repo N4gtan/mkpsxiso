N4gtan/mkpsxiso

commit e5854f23bf0c657159a17fdb75d09e20af980ac9

allow non-standard root attributes and simplify some statements

src/mkpsxiso/main.cpp
@@ -706,7 +706,7 @@ int Main(int argc, char* argv[])
 
 				dirTree->OutputLBAlisting( fp, 0 );
 
-				dirTree->SortDirectoryEntries(global::new_type.has_value() && *global::new_type);
+				dirTree->SortDirectoryEntries(global::new_type.value_or(false));
 				if (!unrefTracks.empty())
 				{
 					iso::DirTreeClass dirTree(unrefTracks, nullptr, "UNREFERENCED TRACKS");
@@ -743,7 +743,7 @@ int Main(int argc, char* argv[])
 
 				dirTree->OutputHeaderListing( fp, 0 );
 
-				dirTree->SortDirectoryEntries(global::new_type.has_value() && *global::new_type);
+				dirTree->SortDirectoryEntries(global::new_type.value_or(false));
 				if (!unrefTracks.empty())
 				{
 					iso::DirTreeClass dirTree(unrefTracks, nullptr, "UNREFERENCED TRACKS");
@@ -883,7 +883,7 @@ int Main(int argc, char* argv[])
 			}
 
 			// Write directory entries
-			dirTree->WriteDirectoryRecords( &writer, root, global::new_type.has_value() && *global::new_type ? dirTree->GetDirCountTotal() : 0 );
+			dirTree->WriteDirectoryRecords( &writer, root, global::new_type.value_or(false) ? dirTree->GetDirCountTotal() : 0 );
 
 			// Write file system descriptors to finish the image
 	        iso::WriteDescriptor( &writer, isoIdentifiers, root, totalLenLBA );
@@ -1225,7 +1225,7 @@ int ParseISOfileSystem(const tinyxml2::XMLElement* trackElement, const fs::path&
 	bool found_da = false;
 	const EntryAttributes defaultAttributes = ReadEntryAttributes(EntryAttributes{}, trackElement->FirstChildElement(xml::elem::DEFAULT_ATTRIBUTES));
 
-	iso::DIRENTRY& root = iso::DirTreeClass::CreateRootDirectory(entries, volumeDate, ReadEntryAttributes(defaultAttributes, directoryTree).HFLAG);
+	iso::DIRENTRY& root = iso::DirTreeClass::CreateRootDirectory(entries, volumeDate, ReadEntryAttributes(EntryAttributes{}, directoryTree));
 	iso::DirTreeClass* dirTree = root.subdir.get();
 
 	if ( !ParseDirectory(dirTree, directoryTree, xmlPath, defaultAttributes, found_da) )
@@ -1239,7 +1239,7 @@ int ParseISOfileSystem(const tinyxml2::XMLElement* trackElement, const fs::path&
 	const int rootLBA = 18+(GetSizeInSectors(pathTableLen)*4);
 
 	// Sort directory entries, calculate tree LBAs and retrieve size of image
-	dirTree->SortDirectoryEntries(global::new_type.has_value() && *global::new_type);
+	dirTree->SortDirectoryEntries(global::new_type.value_or(false));
 	totalLen = dirTree->CalculateTreeLBA(rootLBA);
 
 	if ( !global::QuietMode )

src/dumpsxiso/main.cpp
@@ -364,7 +364,7 @@ static EntryAttributes EstablishXMLAttributeDefaults(tinyxml2::XMLElement* defau
 	defaultAttributesElement->SetAttribute(xml::attrib::XA_GID, defaultAttributes.GID);
 	defaultAttributesElement->SetAttribute(xml::attrib::XA_UID, defaultAttributes.UID);
 	if (defaultAttributes.HFLAG) { // Set only if not zero
-		defaultAttributesElement->SetAttribute(xml::attrib::HIDDEN_FLAG, 0x01);
+		defaultAttributesElement->SetAttribute(xml::attrib::HIDDEN_FLAG, defaultAttributes.HFLAG);
 	}
 
 	return defaultAttributes;
@@ -490,13 +490,12 @@ std::unique_ptr<cd::IsoDirEntries> ParsePathTable(cd::IsoReader& reader, ListVie
 	// Only add the missing directories to the list
     for (int i = 1; i < pathTableList.size(); i++) {
         auto& e = pathTableList[i];
-        if (e.entry.parentDirIndex - 1 == index) {
-			if (!std::any_of(dirEntries->dirEntryList.GetView().begin(), dirEntries->dirEntryList.GetView().end(), [&e](const auto& entry)
-					{
-						return entry.get().identifier == e.name;
-					})) {
-				dirEntries->ReadRootDir(&reader, e.entry.dirOffs);
-			}
+        if (e.entry.parentDirIndex - 1 == index &&
+			!std::any_of(dirEntries->dirEntryList.GetView().begin(), dirEntries->dirEntryList.GetView().end(), [&e](const auto& entry)
+				{
+					return entry.get().identifier == e.name;
+				})) {
+            dirEntries->ReadRootDir(&reader, e.entry.dirOffs);
         }
     } 
 
@@ -574,13 +573,11 @@ std::vector<std::list<cd::IsoDirEntries::Entry>::iterator> processDAfiles(cd::Is
 				continue;
 			}
 			// Skip referenced DA tracks
-			if (tracknum > 2) {
-				if (std::any_of(DAfiles.begin(), DAfiles.end(), [&track](const auto& entry)
-						{
-							return entry->entry.entryOffs.lsb == track.startSector;
-						})) {
-					continue;
-				}
+			if (tracknum > 2 && std::any_of(DAfiles.begin(), DAfiles.end(), [&track](const auto& entry)
+									{
+										return entry->entry.entryOffs.lsb == track.startSector;
+									})) {
+				continue;
 			}
 
 			// Add the unreferenced DA track to the buffer
@@ -599,7 +596,7 @@ std::vector<std::list<cd::IsoDirEntries::Entry>::iterator> processDAfiles(cd::Is
 					break;
 				}
 				reader.ReadBytesDA(sectorBuff, CD_SECTOR_SIZE, true);
-				if (std::memcmp(sectorBuff, emptyBuff, CD_SECTOR_SIZE)) {
+				if (memcmp(sectorBuff, emptyBuff, CD_SECTOR_SIZE)) {
 					entry.entry.entryOffs.lsb--;
 					entry.entry.entrySize.lsb += 2048;
 				}
@@ -701,7 +698,9 @@ void ExtractFiles(cd::IsoReader& reader, const std::list<cd::IsoDirEntries::Entr
 					printf("\n  Creating CDDA files...\n");
 					firstDA = false;
 				}
-				bool isInvalid = !global::cueFile.multiBIN ? !reader.SeekToSector(entry.entry.entryOffs.lsb) : !multiBinSeeker(entry.entry.entryOffs.lsb, entry, reader, global::cueFile);
+				bool isInvalid = !global::cueFile.multiBIN
+					? !reader.SeekToSector(entry.entry.entryOffs.lsb)
+					: !multiBinSeeker(entry.entry.entryOffs.lsb, entry, reader, global::cueFile);
                 auto daOutPath = GetRealDAFilePath(outputPath);
 				auto outFile = OpenScopedFile(daOutPath, "wb");
 
@@ -887,12 +886,12 @@ void WriteXMLGap(const unsigned int numSectors, tinyxml2::XMLElement* dirElement
 	}
 }
 
-void WriteXMLByLBA(std::list<cd::IsoDirEntries::Entry>& files, tinyxml2::XMLElement* dirElement, const fs::path& sourcePath, unsigned int& expectedLBA,
+void WriteXMLByLBA(const std::list<cd::IsoDirEntries::Entry>& files, tinyxml2::XMLElement* dirElement, const fs::path& sourcePath, unsigned int& expectedLBA,
 	EntryAttributeCounters& attributeCounters, cd::IsoReader &reader)
 {
 	fs::path currentVirtualPath; // Used to find out whether to traverse 'dir' up or down the chain
 
-	for (auto& entry : files)
+	for (const auto& entry : files)
 	{
 		// if this is a DA file we are at the end of filesystem
 		if (entry.type != EntryType::EntryDA)
@@ -942,7 +941,7 @@ void WriteXMLByDirectories(const cd::IsoDirEntries* directory, tinyxml2::XMLElem
 {
 	for (const auto& e : directory->dirEntryList.GetView())
 	{
-		auto& entry = e.get();
+		const auto& entry = e.get();
 
 		if (entry.type != EntryType::EntryDA)
 		{

src/mkpsxiso/iso.cpp
@@ -74,20 +74,18 @@ iso::DirTreeClass::~DirTreeClass()
 {
 }
 
-iso::DIRENTRY& iso::DirTreeClass::CreateRootDirectory(EntryList& entries, const cd::ISO_DATESTAMP& volumeDate, const bool hiddenFlag)
+iso::DIRENTRY& iso::DirTreeClass::CreateRootDirectory(EntryList& entries, const cd::ISO_DATESTAMP& volumeDate, const EntryAttributes& attributes)
 {
 	DIRENTRY entry {};
 
 	entry.type		= EntryType::EntryDir;
 	entry.subdir	= std::make_unique<DirTreeClass>(entries);
 	entry.date		= volumeDate;
-	if (!global::new_type.has_value() || !*global::new_type) {
+	if (!global::new_type.value_or(false)) {
 		entry.date.year = volumeDate.year % 0x64; // Root overflows dates past 1999 for games built with old(<2003) mastering tool
 	}
 	entry.length	= 0; // Length is meaningless for directories
-	entry.HF		= hiddenFlag;
-
-	const EntryAttributes attributes; // Leave defaults
+	entry.HF		= attributes.HFLAG;
 	entry.attribs	= attributes.XAAttrib;
 	entry.perms		= attributes.XAPerm;
 	entry.GID		= attributes.GID;
@@ -204,7 +202,7 @@ bool iso::DirTreeClass::AddFileEntry(const char* id, EntryType type, const fs::p
 	entry.id		= std::move(temp_name);
 	entry.type		= type;
 	entry.subdir	= nullptr;
-	entry.HF		= (bool)attributes.HFLAG;
+	entry.HF		= attributes.HFLAG;
 	entry.attribs	= attributes.XAAttrib;
 	entry.perms		= attributes.XAPerm;
 	entry.GID		= attributes.GID;
@@ -301,7 +299,7 @@ iso::DirTreeClass* iso::DirTreeClass::AddSubDirEntry(const char* id, const fs::p
 
 	entry.type		= EntryType::EntryDir;
 	entry.subdir	= std::make_unique<DirTreeClass>(entries, this);
-	entry.HF		= (bool)attributes.HFLAG;
+	entry.HF		= attributes.HFLAG;
 	entry.attribs	= attributes.XAAttrib;
 	entry.perms		= attributes.XAPerm;
 	entry.GID		= attributes.GID;
@@ -473,11 +471,11 @@ bool iso::DirTreeClass::WriteDirEntries(cd::IsoWriter* writer, const DIRENTRY& d
 
 		if ( entry.type == EntryType::EntryDir )
 		{
-			dirEntry->flags = 0x02 | entry.HF;
+			dirEntry->flags = 0x02 | (int)entry.HF;
 		}
 		else
 		{
-			dirEntry->flags = 0x00 | entry.HF;
+			dirEntry->flags = 0x00 | (int)entry.HF;
 		}
 
 		// File length correction for certain file types
@@ -1052,11 +1050,9 @@ void iso::WriteDescriptor(cd::IsoWriter* writer, const iso::IDENTIFIERS& id, con
 	// Setup the root directory record
 	isoDescriptor.rootDirRecord.entryLength = 34;
 	isoDescriptor.rootDirRecord.extLength	= 0;
-	isoDescriptor.rootDirRecord.entryOffs = cd::SetPair32(
-		18+(pathTableSectors*4) );
-	isoDescriptor.rootDirRecord.entrySize = cd::SetPair32(
-		dirTree->CalculateDirEntryLen() );
-	isoDescriptor.rootDirRecord.flags = 0x02 | root.HF;
+	isoDescriptor.rootDirRecord.entryOffs = cd::SetPair32( 18+(pathTableSectors*4) );
+	isoDescriptor.rootDirRecord.entrySize = cd::SetPair32( dirTree->CalculateDirEntryLen() );
+	isoDescriptor.rootDirRecord.flags = 0x02 | (int)root.HF;
 	isoDescriptor.rootDirRecord.volSeqNum = cd::SetPair16( 1 );
 	isoDescriptor.rootDirRecord.identifierLen = 1;
 	isoDescriptor.rootDirRecord.identifier = 0x0;
@@ -1064,11 +1060,9 @@ void iso::WriteDescriptor(cd::IsoWriter* writer, const iso::IDENTIFIERS& id, con
 	isoDescriptor.rootDirRecord.entryDate = root.date;
 
 	isoDescriptor.pathTable1Offs = 18;
-	isoDescriptor.pathTable2Offs = isoDescriptor.pathTable1Offs+
-		pathTableSectors;
+	isoDescriptor.pathTable2Offs = isoDescriptor.pathTable1Offs + pathTableSectors;
 	isoDescriptor.pathTable1MSBoffs = isoDescriptor.pathTable2Offs+1;
-	isoDescriptor.pathTable2MSBoffs =
-		isoDescriptor.pathTable1MSBoffs+pathTableSectors;
+	isoDescriptor.pathTable2MSBoffs = isoDescriptor.pathTable1MSBoffs + pathTableSectors;
 	isoDescriptor.pathTable1MSBoffs = SwapBytes32( isoDescriptor.pathTable1MSBoffs );
 	isoDescriptor.pathTable2MSBoffs = SwapBytes32( isoDescriptor.pathTable2MSBoffs );
 
@@ -1076,10 +1070,10 @@ void iso::WriteDescriptor(cd::IsoWriter* writer, const iso::IDENTIFIERS& id, con
 
 	// Write the descriptor
 	unsigned int currentHeaderLBA = 16;
-	const int ISOver = global::new_type.has_value() && *global::new_type ? 1 : 0;
+	const int ISOver = global::new_type.value_or(false);
 
 	auto isoDescriptorSectors = writer->GetSectorViewM2F1(currentHeaderLBA, 2 + ISOver, cd::IsoWriter::EdcEccForm::Form1);
-	isoDescriptorSectors->SetSubheader(global::new_type.has_value() && *global::new_type ? cd::IsoWriter::SubData : cd::IsoWriter::SubEOL);
+	isoDescriptorSectors->SetSubheader(global::new_type.value_or(false) ? cd::IsoWriter::SubData : cd::IsoWriter::SubEOL);
 
 	isoDescriptorSectors->WriteMemory(&isoDescriptor, sizeof(isoDescriptor));
 

src/mkpsxiso/iso.h
@@ -29,7 +29,7 @@ namespace iso
 
 		fs::path 		srcfile;	/// Filename with path to source file (empty if directory or dummy)
 		EntryType		type;		/// File type (0 - file, 1 - directory)
-		unsigned char	HF;			/// Hidden Flag
+		bool			HF;			/// Hidden Flag
 		unsigned char	attribs;	/// XA attributes, 0xFF is not set
 		unsigned short	perms;		/// XA permissions
 		unsigned short	GID;		/// Owner group ID
@@ -87,7 +87,7 @@ namespace iso
 		DirTreeClass(EntryList& entries, DirTreeClass* parent = nullptr, std::string name = "<root>");
 		~DirTreeClass();
 
-		static DIRENTRY& CreateRootDirectory(EntryList& entries, const cd::ISO_DATESTAMP& volumeDate, const bool hiddenFlag);
+		static DIRENTRY& CreateRootDirectory(EntryList& entries, const cd::ISO_DATESTAMP& volumeDate, const EntryAttributes& attributes);
 
 		void PrintRecordPath();
 

commit 7df18490f58fe04325eb88f3b5550db9d8772980

fixed a regression introduced in commit e5854f23

src/mkpsxiso/main.cpp
@@ -1229,7 +1229,7 @@ int ParseISOfileSystem(const tinyxml2::XMLElement* trackElement, const fs::path&
 	bool found_da = false;
 	const EntryAttributes defaultAttributes = ReadEntryAttributes(EntryAttributes{}, trackElement->FirstChildElement(xml::elem::DEFAULT_ATTRIBUTES));
 
-	iso::DIRENTRY& root = iso::DirTreeClass::CreateRootDirectory(entries, volumeDate, ReadEntryAttributes(EntryAttributes{}, directoryTree));
+	iso::DIRENTRY& root = iso::DirTreeClass::CreateRootDirectory(entries, volumeDate, ReadEntryAttributes(defaultAttributes, directoryTree));
 	iso::DirTreeClass* dirTree = root.subdir.get();
 
 	if ( !ParseDirectory(dirTree, directoryTree, xmlPath, defaultAttributes, found_da) )